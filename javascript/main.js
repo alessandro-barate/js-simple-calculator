9611f16548ccbbb3803f11efe5d2dbcdfb797669
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-09 17:25:49 +0200

    [UPD] Working on the logic

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const number1 = document.getElementById("number-1");
+
+number1.addEventListener("click", function () {
+  const value1 = 1;
+  console.log(value1);
+});
+
+function numberClicked() {}
