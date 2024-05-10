ed8c257d439cdd0f25f52da4a3dd19e7cf7a0a0f
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-10 15:42:12 +0200

    [UPD] Buttons

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,11 @@
 "use strict";
 
-const numbers = document.querySelector(".buttons");
+const numbers = document.querySelectorAll(".button");
 //let dataNumber = numbers.dataset.number;
 
 numbers.addEventListener("click", function () {
   let dataNumber = numbers.dataset.number;
   console.log(dataNumber);
 });
+
+let valueDisplayed = document.getElementById("result");
