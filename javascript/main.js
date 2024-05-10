12408379e7f73411e517b465de800df0471e418d
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-10 16:39:29 +0200

    [UPD] Display

--- javascript/main.js
+++ javascript/main.js
@@ -6,5 +6,8 @@ for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", function () {
     let data = numbers[i].dataset.number;
     console.log(data);
+    let displayId = document.getElementById("result").innerText;
+    displayId = data;
+    console.log(data);
   });
 }
